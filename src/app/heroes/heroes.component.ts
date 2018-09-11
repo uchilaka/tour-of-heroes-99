f3fc34c7f355e7044cec8610dfc216511ce76087
Uchenna Chilaka <uchenna.chilaka@alliancedata.com>
2018-09-11 18:30:30 -0400

    latest updates

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { Hero } from "../hero";
-import { HEROES } from "../mock-heroes";
-import { HeroService } from "../hero.service";
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
-  selector: "app-heroes",
-  templateUrl: "./heroes.component.html",
-  styleUrls: ["./heroes.component.css"]
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = { id: 1, name: "Windstorm" };
   heroes: Hero[];
   selectedHero: Hero;
 
